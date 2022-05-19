1357774a9387a3f65f5c142cb8a051b77680c81e
Alcides-hub <54735993+Alcides-hub@users.noreply.github.com>
2022-05-19 11:16:30 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let myAge = 44;
+let earlyYears = 2;
+earlyYears *= 10.5;
+myAge -= 2;
+let laterYears = myAge;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+myAgeInDogYears = earlyYears + laterYears;
+myName ='ALCIDES'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge}, and ${myAgeInDogYears} years old in dog years.`)
